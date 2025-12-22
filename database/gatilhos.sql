d02cb1786679a8221ddcd5f2dd3b4f599975a8a9
Isaac Fonseca Vasconcelos <20231101110007@ifrn.local>
2025-12-22 09:59:54 -0300

    gatilhos de geração automática de valores

--- database/gatilhos.sql
+++ database/gatilhos.sql
@@ -92,3 +92,100 @@ end;
 //
 
 DELIMITER ;
+
+-- Geração Automática de Valores
+
+
+-- Define automaticamente o status do empréstimo como "pendente"
+-- Caso o status não seja informado pelo sistema ou usuário
+
+DELIMITER //
+
+CREATE TRIGGER trg_auto_status_emprestimo
+BEFORE INSERT ON emprestimos
+FOR each ROW
+BEGIN
+    IF new.status_emprestimo IS NULL THEN
+        SET new.status_emprestimo = 'pendente';
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+
+-- Preenche automaticamente a data de inscrição do usuário
+-- Caso a data não seja informada no momento do cadastro
+
+DELIMITER //
+
+CREATE TRIGGER trg_auto_data_inscricao
+BEFORE INSERT ON usuarios
+FOR each ROW
+BEGIN
+    IF NEW.data_inscricao IS NULL THEN
+        SET NEW.data_inscricao = CURDATE();
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+
+-- Gera automaticamente a data de devolução prevista
+-- Considera 7 dias após a data do empréstimo, se não for informada
+
+DELIMITER //
+
+CREATE TRIGGER trg_auto_data_devolucao_prevista
+BEFORE INSERT ON emprestimos
+FOR EACH ROW
+BEGIN
+    IF NEW.data_devolucao_prevista IS NULL THEN
+        SET NEW.data_devolucao_prevista = DATE_ADD(NEW.data_emprestimo, INTERVAL 7 DAY);
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+
+-- Define automaticamente o valor inicial da multa do usuário
+-- Evita valores nulos no cadastro de novos usuários
+
+DELIMITER //
+
+CREATE TRIGGER trg_auto_multa_padrao
+BEFORE INSERT ON usuarios
+FOR EACH ROW
+BEGIN
+    IF NEW.multa_atual IS NULL THEN
+        SET NEW.multa_atual = 0;
+    END IF;
+END;
+//
+
+DELIMITER ;
+
+
+
+-- Gera automaticamente um resumo padrão para o livro
+-- Caso o campo resumo não seja preenchido no cadastro
+
+DELIMITER //
+
+CREATE TRIGGER trg_auto_resumo_livro
+BEFORE INSERT ON livros
+FOR EACH ROW
+BEGIN
+    IF NEW.resumo IS NULL OR NEW.resumo = '' THEN
+        SET NEW.resumo = CONCAT(
+            'Livro "', NEW.titulo,
+            '" cadastrado automaticamente no sistema.'
+        );
+    END IF;
+END;
+//
+
+
+DELIMITER ;
